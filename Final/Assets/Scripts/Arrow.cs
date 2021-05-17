7a9c4e00364820e49cf723027798c98323eb58a4
srepkin <srepkin@iu.edu>
2021-05-17 04:49:12 -0500

    Made Arrows Stick

--- Final/Assets/Scripts/Arrow.cs
+++ Final/Assets/Scripts/Arrow.cs
@@ -6,6 +6,7 @@ public class Arrow : MonoBehaviour
 {
 
     Rigidbody2D rb;
+    bool hit;
 
     void Start()
     {
@@ -14,7 +15,17 @@ public class Arrow : MonoBehaviour
 
     void Update()
     {
-        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if (hit == false)
+        {
+            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hit = true;
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
     }
 }
