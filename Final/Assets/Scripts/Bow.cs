527d4b35a5ece87051bf1dd3d0fa54fe8ad30ccd
srepkin <srepkin@iu.edu>
2021-05-17 04:18:43 -0500

    Made Bow Follow Cursor

--- Final/Assets/Scripts/Bow.cs
+++ Final/Assets/Scripts/Bow.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        Vector2 bowPosition = transform.position;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - bowPosition;
+        transform.right = direction;
         
     }
 }
