6357e7a1278cd73076a3738800a85ff19997dcc5
srepkin <srepkin@iu.edu>
2021-05-24 20:37:19 -0500

    Fixed bridge script and added animation

--- Final/Assets/Scripts/Bridge.cs
+++ Final/Assets/Scripts/Bridge.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bridge : MonoBehaviour
+public class moveableBridge : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private static bool completed = false;
+    private static bool playing = true;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Target.triggered == true)
+        {
+            completed = true;
+        }
     }
 }
